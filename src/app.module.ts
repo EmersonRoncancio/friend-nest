7a0f33909638ed5ec79d2d223e0ab623d4587480
Emerson Roncancio <roncancioavila@gmail.com>
2024-10-26 19:04:27 -0500

    preparacion de entorno de desarrollo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './common/common.module';
+import { envConfigs } from './common/configs/env.configs';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [envConfigs],
+    }),
+    MongooseModule.forRoot(process.env.MONGODB, {
+      dbName: process.env.DB_NAME,
+    }),
+    AuthModule,
+    CommonModule,
+  ],
   controllers: [],
   providers: [],
 })
