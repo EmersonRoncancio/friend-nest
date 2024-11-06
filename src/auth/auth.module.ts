3a11a276330b970358ec23e9f8f6ce355d996725
Emerson Roncancio <roncancioavila@gmail.com>
2024-11-05 22:47:30 -0500

    configuracion de dependencias de la autenticazion

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,9 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './entities/auth.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   controllers: [AuthController],
@@ -14,6 +17,19 @@ import { User, UserSchema } from './entities/auth.entity';
         schema: UserSchema,
       },
     ]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get('KEYJWT'),
+          signOptions: {
+            expiresIn: '1h',
+          },
+        };
+      },
+    }),
   ],
+  exports: [JwtModule, PassportModule],
 })
 export class AuthModule {}
--- src/common/configs/env.configs.ts
+++ src/common/configs/env.configs.ts
@@ -2,4 +2,5 @@ export const envConfigs = () => ({
   enviroment: process.env.NODE_ENV || 'dev',
   mongodb: process.env.MONGODB,
   port: process.env.PORT || 3001,
+  keyjwt: process.env.KEYJWT,
 });
